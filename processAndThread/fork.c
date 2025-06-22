aad4c630ca4f4639a48b3ca61db1a3cdcf7a8a8e
sueunjo <sueun1201@gmail.com>
2025-06-22 13:24:14 +0900

    fork.c 상세히 이해하면서 주석 추가

--- processAndThread/fork.c
+++ processAndThread/fork.c
@@ -12,22 +12,22 @@ int main (int argc, char ** argv){
 	printf("mainprocess %s :%d\n ", str, getpid());
 	if ( (pid = fork()) < 0 ) //포크 함수 에러시 처리
 		perror("[ERROR] : fork()"); 
-	else if (pid == 0){
+	else if (pid == 0){ //자식 프로세스이면
 		g_var++;
 		var++;
 		printf("Parent %s from Child Process(%d) : %d\n",
-				str, getpid(), getppid());
+				str, getpid(), getppid()); //자식pid는 자식 입장에서 getpid() // 내 부모는 getppid();
 	}
-	else {
+	else { //부모 프로세스면
 		printf("Child %s from Parrent Procees(%d) : %d\n",
-				str, getpid(), pid);
+				str, getpid(), pid); //getpid() 부모 입장에서 난 누구? -> 부모 pid 
 		sleep (1);
+		//sleep 안넣으면 어케되는데? 부모가 먼저 종료될 경우-> 자식은 init(pid 1번)을 부모로 갖게됨
 	}
 
 	//변수 내용 출력
 	printf ("pid = %d, Global var = %d, var = %d\n",
 			getpid(), g_var, var);
-
 
 	return 0;
 }
